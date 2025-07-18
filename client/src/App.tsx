85a22990fa770db8c001a2a10d38cb407728a27d
rodyherrera <contact@rodyherrera.com>
2025-07-18 06:49:03 -0400

    feat: protected routes based on the user's role in the database

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Canva from './pages/protected/Canva';
 import Dashboard from './pages/protected/Dashboard';
 import SignUp from './pages/guest/SignUp';
+import ProtectedRoute from './components/atoms/ProtectedRoute';
 
 const App = () => {
 
@@ -11,7 +12,9 @@ const App = () => {
             <Route path='/canva' element={<Canva />} />
             <Route path='/dashboard' element={<Dashboard />} />
 
-            <Route path='/auth/sign-up' element={<SignUp />} />
+            <Route element={<ProtectedRoute mode='guest' />}>
+                <Route path='/auth/sign-up' element={<SignUp />} />
+            </Route>
         </Routes>
     );
 };
---
+++ client/src/components/atoms/ProtectedRoute/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
+import { useAuthStore } from '../../../stores/authentication';
+import Loader from '../Loader';
+import './ProtectedRoute.css';
+
+type UserRole = 'user' | 'admin';
+
+interface ProtectedRouteProps {
+    mode: 'protect' | 'guest';
+    restrictTo?: UserRole;
+}
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ mode, restrictTo }) => {
+    const location = useLocation();
+    const user = useAuthStore((state) => state.user);
+    const isLoading = useAuthStore((state) => state.isLoading);
+    const isAuthenticated = !!user;
+
+    if(isLoading){
+        return (
+            <main className='auth-loading-main'>
+                <Loader scale='0.6' />
+            </main>
+        );
+    }
+
+    if(mode === 'protect' && restrictTo){
+        if(!isAuthenticated || user.role !== restrictTo) {
+            return <Navigate to="/auth/sign-up" replace />;
+        }
+        return <Outlet />;
+    }
+
+    if(mode === 'protect'){
+        if(!isAuthenticated){
+            return <Navigate to="/sign-in" state={{ from: location }} replace />;
+        }
+        return <Outlet />;
+    }
+
+    if(mode === 'guest'){
+        if(isAuthenticated){
+            return <Navigate to="/dashboard" replace />;
+        }
+        return <Outlet />;
+    }
+
+    return <Navigate to="/" replace />;
+};
+
+export default ProtectedRoute;
