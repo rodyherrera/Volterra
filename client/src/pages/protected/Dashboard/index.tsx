89410b3f6119622f9d59f9fd937133465509bce5
rodyherrera <contact@rodyherrera.com>
2025-07-18 21:10:57 -0400

    feat: simulations of the authenticated user and using skeleton as a preloader

--- client/src/pages/protected/Dashboard/index.tsx
+++ client/src/pages/protected/Dashboard/index.tsx
@@ -1,23 +1,60 @@
+import { useEffect } from 'react';
 import { PiDotsThreeVerticalBold } from "react-icons/pi";
 import DashboardContainer from '../../../components/atoms/DashboardContainer';
 import SimpExampleCover from '../../../assets/images/simulation-example-cover.png';
+import useTrajectoryStore from "../../../stores/trajectories";
+import { PiAtomThin } from 'react-icons/pi';
+import { Skeleton } from '@mui/material'; 
 import './Dashboard.css';
 
 const DashboardPage = () => {
+    const trajectories = useTrajectoryStore((state) => state.trajectories);
+    const isLoading = useTrajectoryStore((state) => state.isLoading);
+    const getTrajectories = useTrajectoryStore((state) => state.getTrajectories);
+
+    useEffect(() => {
+        getTrajectories();
+    }, []);
+
     return (
         <DashboardContainer pageName='Dashboard'>
-            <figure className='simulation-container'>
-                <img className='simulation-image' src={SimpExampleCover} />
-                <figcaption className='simulation-caption-container'>
-                    <div className='simulation-caption-left-container'>
-                        <h3 className='simulation-caption-title'>FCC Test Simulation</h3>
-                        <p className='simulation-last-edited'>Edited 6 hours ago</p>
-                    </div>
-                    <i className='simulation-options-icon-container'>
-                        <PiDotsThreeVerticalBold />
-                    </i>
-                </figcaption>
-            </figure>
+            <div className='trajectories-container'>
+                {(isLoading) && (
+                    (new Array(8).fill(0)).map((_, index) => (
+                        <div className='simulation-container'>
+                            <Skeleton key={index} variant='rounded' width='100%' height='100%' />
+                            <div className='simulation-caption-container'>
+                                <div className='simulation-caption-left-container'>
+                                    <Skeleton variant='text' sx={{ fontSize: '1rem' }} width='60%' />
+                                    <Skeleton variant='text' sx={{ fontSize: '1rem' }} width='40%' />
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                )}
+                {trajectories.map((trajectory) => (
+                    <figure className='simulation-container' key={trajectory._id}>
+                        <div className='simulation-cover-container'>
+                            {true ? (
+                                <i className='simulation-cover-icon-container'>
+                                    <PiAtomThin />
+                                </i>
+                            ) : (
+                                <img className='simulation-image' src={SimpExampleCover} />
+                            )}
+                        </div>
+                        <figcaption className='simulation-caption-container'>
+                            <div className='simulation-caption-left-container'>
+                                <h3 className='simulation-caption-title'>FCC Test Simulation</h3>
+                                <p className='simulation-last-edited'>Edited 6 hours ago</p>
+                            </div>
+                            <i className='simulation-options-icon-container'>
+                                <PiDotsThreeVerticalBold />
+                            </i>
+                        </figcaption>
+                    </figure>
+                ))}
+            </div>
         </DashboardContainer>
     );
 };
