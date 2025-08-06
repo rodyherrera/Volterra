a541137c9a69c2b553c0c7abb5018006baeec004
rodyherrera <contact@rodyherrera.com>
2025-08-06 15:00:48 -0400

    feat(components/organisms): refactor JobsHistoryViewer to DashboardLayout with sidebar navigation

--- client/src/components/atoms/DashboardContainer/index.tsx
+++ client/src/components/atoms/DashboardContainer/index.tsx
@@ -20,57 +20,12 @@
 * SOFTWARE.
 **/
 
-import { HiPlus } from 'react-icons/hi';
-import { IoSearchOutline } from 'react-icons/io5';
-import { MdKeyboardArrowDown } from 'react-icons/md';
-import { IoNotificationsOutline } from "react-icons/io5";
 import './DashboardContainer.css';
 
 const DashboardContainer = ({ children, pageName, className = '' }) => {
 
     return (
         <div className={'dashboard-container '.concat(className + ' wrapper')}>
-            {/*}
-            <article className='dashboard-header-container'>
-                <div className='dashboard-header-left-container'>
-                    <h3 className='dashboard-header-title'>{pageName}</h3>
-                    <div className='clickable-container'>
-                        <i className='clickable-icon-container'>
-                            <HiPlus />
-                        </i>
-                        <span className='clickable-title'>New Folder</span>
-                    </div>
-                </div>
-
-                <div className='dashboard-header-right-container'>
-                    <div className='dashboard-clickables-container'>
-                        {[IoNotificationsOutline].map((Icon, index) => (
-                            <div className='dashboard-clickable-container' key={index}>
-                                <Icon />
-                            </div>
-                        ))}
-                    </div>
-
-                    <div className='search-container'>
-                        <i className='search-icon-container'>
-                            <IoSearchOutline />
-                        </i>
-                        <input placeholder='Search' className='search-input '/>
-                    </div>
-
-                    <div className='create-new-button-container'>
-                        <i className='create-new-button-icon-container'>
-                            <HiPlus />
-                        </i>
-                        <span className='create-new-button-title'>Create</span>
-                        <i className='create-new-button-dropdown-icon-container'>
-                            <MdKeyboardArrowDown />
-                        </i>
-                    </div>
-                </div>
-            </article>
-            */}
-
             <div className={className}>
                 {children}
             </div>
