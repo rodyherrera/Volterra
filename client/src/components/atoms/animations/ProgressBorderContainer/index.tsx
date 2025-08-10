ef912ca10246f74d862ac400bf4f75302cbeeaa5
rodyherrera <contact@rodyherrera.com>
2025-08-10 17:14:42 -0400

    fix: Job loading progress animation returned to the beginning

--- client/src/components/atoms/animations/ProgressBorderContainer/index.tsx
+++ client/src/components/atoms/animations/ProgressBorderContainer/index.tsx
@@ -1,18 +1,14 @@
-/**
-* Copyright (C) Rodolfo Herrera Hernandez. All rights reserved.
-*/
-
 import React, { useMemo } from 'react';
 import './ProgressBorderContainer.css';
 
 interface ProgressBorderContainerProps {
-    children: React.ReactNode;
-    progressBorder: string;
-    animatedProgressBorder?: string; 
-    hasJobs: boolean;
-    shouldHideBorder: boolean;
-    isAnimating?: boolean;
-    className?: string;
+  children: React.ReactNode;
+  progressBorder: string;
+  animatedProgressBorder?: string;
+  hasJobs: boolean;
+  shouldHideBorder: boolean;
+  isAnimating?: boolean;
+  className?: string;
 }
 
 const ProgressBorderContainer: React.FC<ProgressBorderContainerProps> = ({
@@ -24,28 +20,25 @@ const ProgressBorderContainer: React.FC<ProgressBorderContainerProps> = ({
     isAnimating = false,
     className = ''
 }) => {
-    const backgroundImage = useMemo(() => {
-        return animatedProgressBorder || progressBorder;
-    }, [animatedProgressBorder, progressBorder]);
-
     const containerClasses = useMemo(() => {
         return [
             'progress-border-container',
             hasJobs && !shouldHideBorder ? 'progress-border-container--active' : '',
             isAnimating ? 'progress-border-container--animating' : '',
             className
-        ].filter(Boolean).join(' ');
+            ].filter(Boolean).join(' ');
     }, [hasJobs, shouldHideBorder, isAnimating, className]);
 
-    const containerStyle = useMemo(() => ({
-        '--progress-border': backgroundImage
-    } as React.CSSProperties), [backgroundImage]);
+    const containerStyle = useMemo(() =>
+        ({
+            '--progress-border-static': progressBorder,
+            '--progress-border-animated': animatedProgressBorder || progressBorder
+        }) as React.CSSProperties,
+        [progressBorder, animatedProgressBorder]
+    );
 
     return (
-        <div 
-            className={containerClasses}
-            style={containerStyle}
-        >
+        <div className={containerClasses} style={containerStyle}>
             {children}
         </div>
     );
