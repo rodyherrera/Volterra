978a0a5afb6abe510b3612ae9c16f545710c88d2
rodyherrera <contact@rodyherrera.com>
2025-12-16 16:03:22 -0300

    fix: the <Loader /> component does not contain the styles required to be displayed on screen

--- client/src/components/atoms/common/Loader/index.tsx
+++ client/src/components/atoms/common/Loader/index.tsx
@@ -1,38 +1,43 @@
 /**
- * Copyright(c) 2025, The Volterra Authors. All rights reserved.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files(the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
+* Copyright (C) Rodolfo Herrera Hernandez. All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+**/
 
+import React from 'react';
 import Container from '@/components/primitives/Container';
 import './Loader.css';
 
-const Loader = ({ scale }) => {
+interface LoaderProps{
+    scale: number;
+};
+
+const Loader = ({ scale }: LoaderProps) => {
     const loaderItems = Array.from({ length: 12 }, (_, index) => index + 1);
 
-    return(
-        <Container className='p-fixed d-flex content-center items-center'>
+    return (
+        <Container className='d-flex flex-center p-fixed'>
             <Container className='p-relative' style={{ transform: 'scale(' + scale + ')' }}>
                 {loaderItems.map((item) => (
-                    <div
+                    <Container 
                         key={item}
-                        className={`Loader-Item Loader-Item-${item}`} />
+                        className={`p-absolute Loader-Item Loader-Item-${item}`} />
                 ))}
             </Container>
         </Container>
