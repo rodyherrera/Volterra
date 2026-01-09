65a2fb6b8a1f5c747d952ca08e7edbfc5ddf3ef2
rodyherrera <contact@rodyherrera.com>
2026-01-09 18:45:49 -0300

    refactor(server/clean-architecture): creating ApplicationError

---
+++ server-clean-architecture/src/shared/application/errors/ApplicationErrors.ts
@@ -0,0 +1,26 @@
+export default class ApplicationError extends Error{
+    constructor(
+        public readonly code: string,
+        public readonly message: string,
+        public readonly statusCode: number = 500,
+        public readonly isOperational: boolean = true
+    ){
+        super(message);
+    }
+
+    public static badRequest(code: string, message: string): ApplicationError{
+        return new ApplicationError(code, message, 400);
+    }
+
+    public static unauthorized(code: string, message: string): ApplicationError{
+        return new ApplicationError(code, message, 401);
+    }
+
+    public static forbidden(code: string, message: string): ApplicationError{
+        return new ApplicationError(code, message, 403);
+    }
+
+    public static notFound(code: string, message: string): ApplicationError{
+        return new ApplicationError(code, message, 404);
+    }
+};
